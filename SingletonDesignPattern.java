ed767c0e1c8eaebd5674e60c6d9f07d9cbf7d3b2
Aditya <adityavishal903@gmail.com>
2023-03-22 11:56:52 +0530

    Singleton Design Pattern

--- SingletonDesignPattern.java
+++ SingletonDesignPattern.java
@@ -1,19 +1,25 @@
-public class SingletonDesignPattern {
-    
-    // Private static variable to hold the single instance of the class
-    private static SingletonDesignPattern instance = null;
-    
-    // Private constructor to prevent instantiation of the class from outside
-    private SingletonDesignPattern() {}
-    
-    // Public static method to get the single instance of the class
-    public static SingletonDesignPattern getInstance() {
-        // Check if the instance has already been created
-        if (instance == null) {
-            // Create a new instance of the class
-            instance = new SingletonDesignPattern();
-        }
-        // Return the single instance of the class
-        return instance;
-    }
+// Double Checked Locking based Java implementation of
+// singleton design pattern
+class SingletonDesignPattern
+{
+	private static volatile SingletonDesignPattern obj = null;
+
+	private SingletonDesignPattern() {}
+
+	public static SingletonDesignPattern getInstance()
+	{
+		if (obj == null)
+		{
+			// To make thread safe
+			synchronized (SingletonDesignPattern.class)
+			{
+				// check again as multiple threads
+				// can reach above step
+				if (obj==null)
+					obj = new SingletonDesignPattern();
+			}
+		}
+		return obj;
+	}
 }
+
